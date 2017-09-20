ba43cd007a553583c561e0354f27f421258a265b
Alex Knopp <coda@knoppys.co.uk>
2017-09-20 08:50:04 +0100

    First and likely last commit

--- knoppys-delete-comments.php
+++ knoppys-delete-comments.php
@@ -10,7 +10,13 @@ License URI:       http://www.gnu.org/licenses/gpl-2.0.html
 GitHub Plugin URI: https://github.com/knoppys/deleteComments.git
 GitHub Branch:     master
 */
+/*
+How to use this plugin
 
+1. Install it like any other plugin, you may need to download the file and zip it into a folder with the same name as the file (knoppys-delete-comments.zip)
+2. Once activated, edit the plugin file and add your own post ID's to list below.
+3. Go to your website homepage, add ?deletereviews=go onto the end of your URL and click go. This will run the function below. 
+*/
 
 //Add the function into the WP Head
 add_action('wp_head', 'delete_reviews');
